91a5903bad63eb345811132b9667565210e9f1c1
admin <admin@W7-FRG-02.mesa.gmu.edu>
2018-03-14 13:47:37 -0400

    uploaded plot

--- potomac_total_water/images/git_push_new.bat
+++ potomac_total_water/images/git_push_new.bat
@@ -1,4 +1,2 @@
-git pull
-git add --all
-git commit -m "uploaded plot"
-git push
+cd  C:\Users\admin\MasonFloodHazardsResearchLab.github.io\potomac_total_water\images
+git-cmd.exe  git push
---
+++ potomac_total_water/images/gitcode.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 13:34:53 2018
+
+@author: admin
+"""
+import git
+git pull
+git add --all
+git commit -m "uploaded plot"
+git push
